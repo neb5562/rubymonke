a7fb248d75978b4276c48c6cc0f2210e15bcfa3f
neb5562 <gnebieridzs@unisens.ge>
2022-03-26 00:01:47 +0400

    0.1 Being Meta Exercises

---
+++ 0.1 Being Meta/exercise1.rb
@@ -0,0 +1,4 @@
+# Ruby's String has no method that allows you to split a sentence into an Array of words.
+#  Let's add a method called words to String to do just this. If you're not sure about
+#     how to split a string, take a look at our lesson on string manipulation in the 
+#     "Ruby Primer" first.
