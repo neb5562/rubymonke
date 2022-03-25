9afec27f3b3b55348bf540637539780898813e8e
neb5562 <gnebieridzs@unisens.ge>
2022-03-25 23:59:39 +0400

    1.0 Send Exercises

---
+++ 1.0 Send/exercise1.rb
@@ -0,0 +1,13 @@
+# Ruby gives a convenient way for you to call any method on an object by using the send
+# method. send takes, as its first argument, the name of the method that you want to call. 
+# This name can either be a symbol or a string.
+
+class Nomad
+    def initialize(glider)
+      @glider = glider
+    end
+  
+    def do(action)
+      # Write your commands here
+    end
+end
---
+++ 1.0 Send/exercise4.rb
@@ -0,0 +1,8 @@
+# That means if I pass in an array of numbers, and specify the data_type as s 
+# (which stands for 'string'), the method should return another array with all
+#  elements converted to strings. This can be done by calling to_s on every element 
+# in the array.
+
+def relay(array, data_type)
+    # Write your code here
+end
