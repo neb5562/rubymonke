ddf94f5318db9382ccd5f87d9386455bd5e7374e
neb5562 <gnebieridzs@unisens.ge>
2022-03-25 23:49:56 +0400

    1.2 Define Method Exercises

---
+++ 1.2 Define Method/exercise1.rb
@@ -0,0 +1,8 @@
+# Create a class Monk which can meditate on life, the universe or everything. It 
+# should have three methods meditate_on_life, meditate_on_the_universe & 
+# meditate_on_everything and returns strings of "I know the meaning of life", "I know 
+# the meaning of the universe", and "I know the meaning of everything", respectively.
+
+class Monk
+    # put your code here
+end
