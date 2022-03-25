6d9fa45fe890a19286ff413d592b14ec39a0985f
neb5562 <gnebieridzs@unisens.ge>
2022-03-26 00:32:36 +0400

    2.2 exercise 1 solution

--- 2.2 Tap In Deeper/exercise1.rb
+++ 2.2 Tap In Deeper/exercise1.rb
@@ -4,9 +4,23 @@
 # The editor object might be constructed something like this
 # e = Editor.new("class Foo; end")
 
+# The editor object might be constructed something like this
+# e = Editor.new("class Foo; end")
+e = Editor.new("class Foo; end")
+
 def auto_complete(e)
 	e.cursor.read do |word|
-    # define your filters here
+        if word == "\n"
+          throw :eol
+        end
+      if e.template.user_classes.include? word
+          l = Module.const_get(word.to_s).methods
+            build_suggestion(l)
+        end
+      if e.template.context.methods.include? word
+        l = e.template.context.method(word).parameters
+            build_suggestion(l)
+        end
 	end
 end
 
