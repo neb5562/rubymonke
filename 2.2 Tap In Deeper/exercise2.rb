cd924bafc9b9ee71b09e715ffcabd2173d1280b5
neb5562 <gnebieridzs@unisens.ge>
2022-03-26 00:34:39 +0400

    2.2 exercise 2 solution

--- 2.2 Tap In Deeper/exercise2.rb
+++ 2.2 Tap In Deeper/exercise2.rb
@@ -2,4 +2,5 @@
 # String and the name of the instance variable defined on that class (again, as a String) 
 # whose value it should return.
 def inspect_instance_variable(class_name, variable)
+    return Module.const_get(class_name.to_s).new.instance_variable_get("@" + variable)
 end
