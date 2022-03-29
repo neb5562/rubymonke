e4755e6fea1437e3fb4942293947edc2279c7c5b
neb5562 <gnebieridzs@unisens.ge>
2022-03-29 20:44:25 +0400

    removed comments, fixed spaces, fixed variable names ABC

--- 2.2 Tap In Deeper/exercise2.rb
+++ 2.2 Tap In Deeper/exercise2.rb
@@ -1,6 +1,3 @@
-# Write a method inspect_instance_variable that accepts the name of a class as a 
-# String and the name of the instance variable defined on that class (again, as a String) 
-# whose value it should return.
 def inspect_instance_variable(class_name, variable)
     return Module.const_get(class_name.to_s).new.instance_variable_get("@" + variable)
 end
