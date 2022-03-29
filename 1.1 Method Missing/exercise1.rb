37dc8fed5b92b8d7ffd3e81b33ff5a11e068d973
neb5562 <gnebieridzs@unisens.ge>
2022-03-29 20:25:51 +0400

    removed comments from exercise1.rb

--- 1.1 Method Missing/exercise1.rb
+++ 1.1 Method Missing/exercise1.rb
@@ -1,10 +1,3 @@
-# Well, what you need to understand is any method call in Ruby is actually a message 
-# being passed to that object. One way to demonstate is to look at the method_missing 
-# method. Let's take a look at it in it's most basic form and see if we can use it to 
-# solve our previous problem. You need to define a method in your class which accepts 
-# one argument (maybe called sym) which will have the name of the method that was called 
-# on your object (as a symbol).
-
 class Spy
     def method_missing sym
     end
