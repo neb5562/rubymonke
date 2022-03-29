dc088778e64519b6e26be71617aa6957a2344469
neb5562 <gnebieridzs@unisens.ge>
2022-03-29 20:37:57 +0400

    fixed align code

--- 1.0 Send/exercise3.rb
+++ 1.0 Send/exercise3.rb
@@ -2,8 +2,8 @@ class Initiate
   def initialize(secretive_monk)
     @secretive_monk = secretive_monk
   end
-  
-  def	meaning_of_life
+
+  def meaning_of_life
     @secretive_monk.send(:meaning_of_life)
   end
 end
