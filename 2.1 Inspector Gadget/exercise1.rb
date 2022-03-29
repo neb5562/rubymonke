cfca2b210fef2d215af581ad869e2112c684492c
neb5562 <gnebieridzs@unisens.ge>
2022-03-29 20:16:24 +0400

    fixed variable name. aligned code.

--- 2.1 Inspector Gadget/exercise1.rb
+++ 2.1 Inspector Gadget/exercise1.rb
@@ -1,32 +1,22 @@
-# Finish the method called auto_complete that accepts an Editor object and use the
-# build_suggestion method that accepts an Array from 1) Object#methods 2)
-#  Method#parameters and incorporate all of the filters explained above, making 
-#  the relevant specs pass.
-
-# We've completed the first filter for you as an example.
-
 editor = Editor.new("class Foo; end")
 
 def auto_complete(editor)
-	editor.cursor.read do |word|
+  editor.cursor.read do |word|
     if word == "\n"
       throw :eol
     end
       if editor.template.user_classes.include? word
-        l = eval(word.to_s).methods
-        build_suggestion(l)
+        methods = eval(word.to_s).methods
+        build_suggestion(methods)
     end
       if editor.template.context.methods.include? word
-        l = editor.template.context.method(word).parameters
-        build_suggestion(l)
+        methods = editor.template.context.method(word).parameters
+        build_suggestion(methods)
       end
-    # define the other two filters like the above. 
-	end
+  end
 end
 
-# This takes an Array of suggestions and displays them.
-# It could either be an Array of methods, like the ones returned
-# by Object#methods or an Array returned by Method#parameters.
+
 def build_suggestion(list)
   Suggestion.new(list).display
 end
